4148e7ed9ad3ef565ce1b0224c059e490e536ec6
CrioUser <crio-user@criodo.com>
2024-02-22 21:07:53 +0530

    ME_BUILDOUT_XCOMPANY_MODULE_XCOMPANY

--- python/src/company/Company.py
+++ python/src/company/Company.py
@@ -16,6 +16,50 @@ class Company:
     # Please define all the methods required here as mentioned in the XCompany BuildOut Milestone before implementing the logic.
     # This will ensure that the project can be compiled successfully.
 
+    # Method 1: register_employee
+    def register_employee(self, employee_name: str, gender: Gender) -> None:
+        emp = Employee(employee_name, gender)
+        self._employee_book[employee_name] = emp
+
+    # Method 2: get_employee
+    def get_employee(self, employee_name: str) -> Employee:
+        try:
+            return self._employee_book[employee_name]
+        except:
+            return None
+
+    # Method 3: delete_employee
+    def delete_employee(self, employee_name: str) -> None:
+        self._employee_book.pop(employee_name)
+
+    # Method 4: assign_manager
+    def assign_manager(self, employee_name: str, manager_name: str) -> None:
+        emp = self._employee_book[employee_name]
+        man = self._employee_book[manager_name]
+        man._direct_reports.append(emp)
+        emp.assign_manager(man)
+
+    # Method 5: get_direct_reports
+    def get_direct_reports(self, manager_name: str) -> List[Employee]:
+        man = self._employee_book[manager_name]
+        return man.get_direct_reports()
+
+    # Method 6: get_team_mates
+    def get_team_mates(self, employee_name: str) -> List[Employee]:
+        emp = self._employee_book[employee_name]
+        return emp.get_team_mates()
+
+    # Method 7: get_employee_hierarchy
+    def get_employee_hierarchy(self, manager_name: str) -> List[List[Employee]]:
+        man = self._employee_book[manager_name]
+        dr_list = man.get_direct_reports()
+        result = [[man]]
+        result.append(dr_list)
+        for ele in dr_list:
+            temp = ele.get_direct_reports()
+            if temp is not None and temp != []:
+                result.append(temp)
+        return result
 
 
 
--- python/src/company/Employee.py
+++ python/src/company/Employee.py
@@ -6,6 +6,8 @@ class Employee:
     def __init__(self,name: str , gender: Gender) -> None:
         self._name = name
         self._gender = gender
+        self._manager = None
+        self._direct_reports = []
 
     def get_name(self) -> str:
         return self._name
@@ -17,6 +19,21 @@ class Employee:
     # Please define all the methods required here as mentioned in the XCompany BuildOut Milestone before implementing the logic.
     # This will ensure that the project can be compiled successfully.
 
+    # Method 1: assign_manager
+    def assign_manager(self, employee) -> None:
+        self._manager = employee
+
+    # Method 2: get_direct_reports
+    def get_direct_reports(self) -> List:
+        return self._direct_reports
+
+    # Method 3: get_team_mates
+    def get_team_mates(self) -> List:
+        man = self._manager
+        dr_list = man.get_direct_reports()
+        result = [man]
+        result.extend(dr_list)
+        return result
 
     def __repr__(self) -> str:
         return f'Employee [name={self._name}, gender={self._gender.value}]'
